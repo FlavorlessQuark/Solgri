8830f2f735fdab6557a0bec14fd38ca88b77c44e
FlavorlessQuark <flavorlessquark@gmail.com>
2026-05-10 02:49:12 -0700

    Added db and initial state

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ from dotenv import dotenv_values
 env = dotenv_values(".env")
 # Constants for IBM COS values
 COS_ENDPOINT = env["ENDPOINTS"]
-COS_API_KEY_ID = env["IBMAPIKEY"]
+COS_API_KEY_ID = env["IBM_APIKEY"]
 COS_INSTANCE_CRN = env["RESOURCE_INSTANCE_ID"]
 BUCKET_NAME = env["BUCKET_NAME"]
 
@@ -19,10 +19,15 @@ cos_client = ibm_boto3.client("s3",
 )
 
 def save_data_to_cos(data,item_name):
-    try:
-        cos_client.put_object(Bucket=BUCKET_NAME, Key=item_name, Body=data)
-        print(f"Data saved to {BUCKET_NAME}/{item_name}")
-    except ClientError as be:
-        print("CLIENT ERROR: {0}\n".format(be))
-    except Exception as e:
-        print("Unable to save data: {0}".format(e))
+    json.dump(data, open(f"./data/{item_name}.json", "w"), indent=4)
+
+
+    # Doesn't work , get 404 Error, but the code is correct according to IBM documentation, so it might be an issue with the bucket or the endpoint
+    # print(f"Saving data to {BUCKET_NAME}/{item_name}...")
+    # try:
+    #     cos_client.put_object(Bucket=BUCKET_NAME, Key=item_name, Body=data)
+    #     print(f"Data saved to {BUCKET_NAME}/{item_name}")
+    # except ClientError as be:
+    #     print("CLIENT ERROR: {0}\n".format(be))
+    # except Exception as e:
+    #     print("Unable to save data: {0}".format(e))
